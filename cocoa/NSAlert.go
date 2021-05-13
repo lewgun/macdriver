558ab07ff3a76387a2113167277b67fff3759ff9
lewgun <lewgun@163.com>
2021-05-13 18:02:51 +0800

    add NSAlert

---
+++ cocoa/NSAlert.go
@@ -0,0 +1,46 @@
+package cocoa
+
+import (
+	"github.com/progrium/macdriver/core"
+	"github.com/progrium/macdriver/objc"
+)
+
+type NSAlert struct {
+	objc.Object
+}
+
+var nsAlert = objc.Get("NSAlert")
+
+func NSAlert_New() NSAlert {
+	return NSAlert{nsAlert.Alloc().Init()}
+}
+
+
+//
+//func (i NSAlert) Title() string {
+//	return i.Get("title").String()
+//}
+
+func (i NSAlert) SetTitle(s string) {
+	i.Get("window").SetTitle(s)
+}
+
+func (i NSAlert) SetMessageText(s string) {
+	i.Set("setMessageText:" ,core.String(s))
+}
+
+
+func (i NSAlert) Icon() NSImage {
+	return NSImage{i.Get("icon")}
+}
+
+func (i NSAlert) SetIcon(obj NSImage) {
+	i.Set("icon:", obj)
+}
+func (i NSAlert)AddButtonWithTitle(s string) {
+	i.Set("addButtonWithTitle:" ,core.String(s))
+}
+
+func (i NSAlert) RunModal() {
+	i.Send("runModal")
+}
